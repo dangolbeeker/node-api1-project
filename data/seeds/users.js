8a99ffcf9849ff78163025d14119fd4476754ba1
dangolbeeker <scottb305@outlook.com>
2020-01-13 18:09:11 -0500

    CRA

--- data/seeds/users.js
+++ data/seeds/users.js
@@ -8,8 +8,24 @@ exports.seed = function(knex) {
           bio: 'Gardener and poet. Married to Rose Cotton',
         },
         {
+          name: 'Scott Beeker',
+          bio: 'Software Engineer',
+        },
+        {
           name: 'Frodo Baggins',
           bio: 'The ring bearer',
+        },
+        {
+          name: 'Ralph Lauren',
+          bio: 'im not real',
+        },
+        {
+          name: 'Mykeal Kenny',
+          bio: 'Devops',
+        },
+        {
+          name: 'Torrie Foutz',
+          bio: 'Hand of Death',
         },
       ]);
     });
