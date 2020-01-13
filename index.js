46bf9e538faa3b5fb78893c4fc8d9fc3de3d7f29
dangolbeeker <scottb305@outlook.com>
2020-01-13 17:11:27 -0500

    Get All Users

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // implement your API here
+
+const express = require('express');
+
+const db = require('./data/db');
+
+const server = express();
+
+// server.use(express.json());
+
+
+
+server.get('/', (req, res) => {
+    res.send('')
+});
+
+server.post('/api/users', (req, res) => {
+    res.send();
+});
+
+
+server.get('/api/users', (req, res) => {
+    //.find() returns a promise 
+    db.find()
+    .then(users => {
+        res.status(200).json(users);
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'error getting users'});
+    });
+});
+
+
+
+const port = 8000;
+server.listen(port, () => console.log('API ONLINE'));
